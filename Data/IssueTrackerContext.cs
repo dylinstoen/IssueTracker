a85d7cdee9ec51636e18ef1752024be3a7acbc94
dylin <dstoen20@gmail.com>
2026-04-19 22:54:53 -0700

    first commit

---
+++ Data/IssueTrackerContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+using IssueTracker.Models;
+
+namespace IssueTracker.Data {
+    public class IssueTrackerContext : DbContext {
+        public IssueTrackerContext(DbContextOptions<IssueTrackerContext> options) : base(options) {
+        }
+        public DbSet<Issue> Issues { get; set; } = default!;
+    }
+}
---
+++ Models/Issue.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace IssueTracker.Models {
+    public class Issue {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Title { get; set; } = string.Empty;
+        
+        [StringLength(100)]
+        public string? Description { get; set; }
+
+        [Required]
+        public string Priority { get; set; } = "Medium";
+
+        [Required]
+        public string Status { get; set; } = "Open";
+
+        [Display(Name="Created At")]
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        
+    }
+}
